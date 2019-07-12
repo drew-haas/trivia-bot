2702cf079ce8d18488ea9dedd2c0a569b99ae46d
Andrew Haas <ahaas3@apple.com>
2019-07-12 15:59:05 -0700

    update data and functionality

--- index.js
+++ index.js
@@ -1,13 +1,146 @@
 /**
- * A Bot for Slack!
+ *
+ * A Trivia Bot for Slack!
+ *
+ * Local Testing in terminal:
+ * xoxb-your-token npm start
+ *
  */
 
+/**
+ * Data for Now:
+ */
+const dataAll = {
+    dataCategory: 'dataAll',
+    questions: [
+        {
+            question: 'Name as many countries as you can in 30 seconds.',
+            answers: [
+                'united states', // todo: decide a way to group answers that have multiple spelling
+                'USA',
+                'United States of America',
+                'canada',
+                'mexico',
+                'ghana',
+                'spain',
+                'brasil', // todo: decide a way to group answers that have multiple spelling
+                'brazil',
+                'thailand',
+                'england',
+                'france',
+                'ireland',
+            ]
+        },
+        {
+            question: 'Name as many of the wealthiest people on the Forbes 100 list in 30 seconds.',
+            answers: [
+                'Jeff Bezos',
+                'Bill Gates',
+                'Warren Buffet',
+                'Bernard Arnault',
+                'Carlos Slim Helu',
+                'Amancio Ortega',
+                'Larry Ellison',
+                'Mark Zuckerberg',
+                'Michael Bloomberg',
+                'Larry Page',
+            ]
+        },
+        {
+            question: 'Name as many states in the USA as you can in 30 seconds.',
+            answers: [
+                'Alaska',
+                'Arkansas',
+                'Arizona',
+                'Alabama',
+                'California',
+                'Colorado',
+                'Connecticut',
+                'Deleware',
+                'Ohio',
+            ]
+        },
+        {
+            question: 'Name as many elements on the periodic table as you can.',
+            answers: [
+                'Gold',
+                'Germanium',
+                'Gallium',
+            ]
+        }
+    ]
+};
+
+const dataGeneral = {
+    dataCategory: 'dataGeneral',
+    questions: [
+        {
+            question: 'Data General Question',
+            answers: [
+                'Cupertino',
+                'Calgary',
+                'Sao Paulo',
+            ]
+        }
+    ]
+};
+
+const dataSports = {
+    dataCategory: 'dataSports',
+    questions: [
+        {
+            question: 'Name as many MLS teams as you can in 30 Seconds.',
+            answers: [
+                'Crew',
+                'Earthquakes',
+                'Fire',
+                'Red Bulls'
+            ]
+        }
+    ]
+};
+
+const dataMovies = {
+    dataCategory: 'dataMovies',
+    questions: [
+        {
+            question: 'Top 100 Movies on IMDB.. Go!',
+            answers: [
+                'Shawshank Redemption',
+                'Django',
+                'City of God',
+            ]
+        }
+    ]
+};
+
+const dataCm = {
+    dataCategory: 'dataCm',
+    questions: [
+        {
+            question: 'Name as many CM offices as you can in 30 seconds.',
+            answers: [
+                'Cupertino',
+                'Calgary',
+                'Sao Paulo',
+            ]
+        }
+    ]
+};
+
+/**
+ *  Helper Functions
+ *
+ * TODO: move to separate file
+*/
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 /**
  * Define a function for initiating a conversation on installation
  * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
  */
-
 function onInstallation(bot, installer) {
     if (installer) {
         bot.startPrivateConversation({user: installer}, function (err, convo) {
@@ -25,7 +158,6 @@ function onInstallation(bot, installer) {
 /**
  * Configure the persistence options
  */
-
 var config = {};
 if (process.env.MONGOLAB_URI) {
     var BotkitStorage = require('botkit-storage-mongo');
@@ -41,7 +173,6 @@ if (process.env.MONGOLAB_URI) {
 /**
  * Are being run as an app or a custom integration? The initialization will differ, depending
  */
-
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     //Treat this as a custom integration
     var customIntegration = require('./lib/custom_integrations');
@@ -79,22 +210,100 @@ controller.on('rtm_close', function (bot) {
 /**
  * Core bot logic goes here!
  */
-// BEGIN EDITING HERE!
+// Global Trivia Variables
+let currentQuestion;
 
+// Entering a Channel
 controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+    bot.reply(message, "I'm here let's play!");
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+// Friendly Commands
+// options:
+// hello, hi, whats up, yo
+controller.hears(['hi', 'whats up', 'yo'], ['direct_message', 'mention', 'direct_mention'], function (bot, message) {
+    bot.reply(message, 'Hi There!');
 });
+
+// Play Commands
+// options:
+// play all -- only one working right now
+// play general
+// play sports
+// play movies
+// play cm -- play critical mass
+controller.hears('(play all|play general|play sports|play movies|play cm)', ['direct_mention', 'mention', 'direct_message'], function(bot, message) { // TODO: make this take in multiple play options
+    // set correct data
+    var data = dataAll;
+
+    if (message.text === 'play general') {
+        data = dataGeneral;
+    } else if (message.text === 'play sports') {
+        data = dataSports;
+    } else if (message.text === 'play movies') {
+        data = dataMovies;
+    } else if (message.text ===  'play cm') {
+        data = dataCm;
+    }
+
+    // get a random question from data
+    currentQuestion = getRandomInt(data.questions.length);
+
+    // reply to the message with a trivia question
+    bot.reply(message, 'Playing All Categories!');
+    bot.reply(message, data.questions[currentQuestion].question);
+
+    // start round
+    // let the rest of the functionality for each round come from here:
+    startRound(data, currentQuestion);
+});
+
+// Stop Commands
+controller.hears(['STOP'], ['direct_mention', 'mention', 'message', 'direct_message'], function(bot, message) {
+    console.log(message.text);
+    bot.reply(message, 'round stopped!');
+    // stop
+});
+
+function startRound(roundData, currentQuestion) {
+    var answerLength = roundData.questions[currentQuestion].answers.length; // number of answers
+    var roundType = roundData.dataCategory; // data category
+    var roundAnswers = roundData.questions[currentQuestion].answers;
+
+    // if bot hears one of the answers.. track who answered it.. notify it was answered..
+    console.log(roundData.questions[currentQuestion].answers);
+    controller.hears(roundData.questions[currentQuestion].answers, ['ambient'], function (bot, message) {
+        // correct answer
+        console.log('correct');
+        bot.reply(message, 'correct');
+    });
+
+    // if bot hears same answer twice.. notify it has been answered by whom
+
+    // start a timer with end round callback
+    // each timer will be 30 seconds for now
+    setTimeout(30000, endRound);
+}
+
+function endRound(roundType, currentQuestion) {
+    console.log('end round');
+
+    // stop tracking
+
+    // notify round has ended
+    // bot.reply(message, 'Round ended!');
+
+    // notify
+
+    // add missed function
+}
 
 
 /**
  * AN example of what could be:
  * Any un-handled direct mention gets a reaction and a pat response!
  */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
+// controller.on('direct_message,mention,direct_mention', function (bot, message) {
 //    bot.api.reactions.add({
 //        timestamp: message.ts,
 //        channel: message.channel,
@@ -105,4 +314,7 @@ controller.hears('hello', 'direct_message', function (bot, message) {
 //        }
 //        bot.reply(message, 'I heard you loud and clear boss.');
 //    });
-//});
+// });
+
+
+// Starts Round functionality
